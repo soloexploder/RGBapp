fa9ee699a7bfa3aece3aba232c9bdad711c965c6
DaXant <soloexploder@gmail.com>
2021-03-27 16:43:44 +0300

    Initial Commit

---
+++ RGBapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBapp
+//
+//  Created by MacBook on 27.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
