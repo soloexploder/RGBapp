72282710dff8a821a4d30de00b5298e64ae86c96
DaXant <soloexploder@gmail.com>
2021-03-29 08:17:18 +0300

    colorsApp

--- RGBapp/ViewController.swift
+++ RGBapp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelValue()
+        
+        colorView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func sliderAction() {
+        
+        labelValue()
+        
+        self.colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                                 green: CGFloat(greenSlider.value),
+                                                 blue: CGFloat(blueSlider.value),
+                                                 alpha: 1)
+    }
+    
+    private func labelValue() {
+        redLabel.text = String(format: "%.2f",redSlider.value)
+        greenLabel.text = String(format: "%.2f",greenSlider.value)
+        blueLabel.text = String(format: "%.2f",blueSlider.value)
+    }
 }
 
